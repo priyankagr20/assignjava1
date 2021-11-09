d7c65f13127746caf81191e9f4ef669c7cb59097
priyankagr20 <70756560+priyankagr20@users.noreply.github.com>
2021-11-09 14:19:32 +0530

    Add files via upload
    day1 assignment java

---
+++ java1.java
@@ -0,0 +1,15 @@
+/*
+Java is the name of an island in Indonesia where coffee is produced.
+*/
+
+class test{
+    public static void main(String[]args){
+        System.out.print("Hello coders");
+        System.out.print("coders")
+    }
+}
+/*
+String[] args
+String []args
+String args[]
+*/
